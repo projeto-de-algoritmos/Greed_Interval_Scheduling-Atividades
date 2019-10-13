0bd7ad538566ee887cad1c8703f14f91c5a6e9ce
joaovitorml <joao.lemos4000@gmail.com>
2019-10-13 20:05:38 -0300

    Converted previous C++ code into python.
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- Interval_Scheduling.py
+++ Interval_Scheduling.py
@@ -1,14 +1,117 @@
-class atividade:
-    def __init__(self, comeco, final):
-        self.comeco = bool
-        self.final = bool
-    def __str__(self):
-        return self.comeco 
-        return self.final
+from dataclasses import dataclass
 
-    def comparar(self, s1, s2):
-        return s1.final < s2.final
-    def comparar2(self, s1, s2):
-        return s1.comeco < s2.comeco
-    def comparar3(self, s1, s2):
-        return (s1.final - s1.comeco) < (s1.final - s2.comeco)
+@dataclass
+class Atividade:
+    start: int
+    finish: int
+
+
+def DeadLineActivities(activities):
+
+    n = len(activities)
+    x=0
+    y=1
+    while(x<len(activities)):
+        y=0
+        while(y<len(activities)):
+            if(activities[x].finish<activities[y].finish):
+                temp = activities[y].finish
+                activities[y].finish = activities[x].finish
+                activities[x].finish = temp
+                temp = activities[y].start
+                activities[y].start = activities[x].start
+                activities[x].start = temp
+            y=y+1
+        x=x+1
+    
+
+    print("Atividades que podem ser feitas (pelo deadline time): ")
+
+    i = 0
+    print("(", activities[i].start, ",", activities[i].finish, ")")
+
+    j = 1
+    while(j<len(activities)):
+        if(activities[j].start >= activities[i].finish):
+            print("(", activities[j].start, ",", activities[j].finish, ")")
+            i = j
+        j = j+1      
+
+def StartLineActivities(activities):
+
+    n = len(activities)
+
+    x=0
+    y=1
+    while(x<len(activities)):
+        y=0
+        while(y<len(activities)):
+            if(activities[x].start<activities[y].start):
+                temp = activities[y].finish
+                activities[y].finish = activities[x].finish
+                activities[x].finish = temp
+                temp = activities[y].start
+                activities[y].start = activities[x].start
+                activities[x].start = temp
+            y=y+1
+        x=x+1
+
+    print("Atividades que podem ser feitas (pelo start time): ")
+
+    i = 0
+    print("(", activities[i].start, ",", activities[i].finish, ")")
+
+    j = 1
+    while(j<len(activities)):
+        if(activities[j].start >= activities[i].finish):
+            print("(", activities[j].start, ",", activities[j].finish, ")")
+            i = j
+        j = j+1  
+
+def SlackActivities(activities):
+
+    n = len(activities)
+
+    x=0
+    y=1
+    while(x<len(activities)):
+        y=0
+        while(y<len(activities)):
+            if((activities[x].finish - activities[x].start) < (activities[y].finish - activities[y].start)):
+                temp = activities[y].finish
+                activities[y].finish = activities[x].finish
+                activities[x].finish = temp
+                temp = activities[y].start
+                activities[y].start = activities[x].start
+                activities[x].start = temp
+            y=y+1
+        x=x+1
+    
+    print("Atividades que podem ser feitas (pelo slack): ")
+
+    i = 0
+    print("(", activities[i].start, ",", activities[i].finish, ")")
+
+    j = 1
+    while(j<len(activities)):
+        if(activities[j].start >= activities[i].finish):
+            print("(", activities[j].start, ",", activities[j].finish, ")")
+            i = j
+        j = j+1  
+
+
+activities = [Atividade(5,9), Atividade(1,2), Atividade(0,6), Atividade(5,7), Atividade(8,9)]
+
+print("Atividades a serem analisadas (start time, deadline): \n")
+
+n = 0
+while(n<len(activities)):
+    print("Atividade",n+1,": ","(", activities[n].start, ",", activities[n].finish, ")")
+    n = n +1
+
+print("\n")
+DeadLineActivities(activities)
+print("\n")
+StartLineActivities(activities)
+print("\n")
+SlackActivities(activities)
